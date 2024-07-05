764f264258ff97dffa89c5ac290e64bb06afca12
EmmyDunny1 <emmanueloluwadunsin.30@gmail.com>
2024-07-05 14:00:29 +0100

    added

--- weather.js
+++ weather.js
@@ -1,6 +1,8 @@
+
+
 const apiKey = "5914f4f8ab9db40093bc1d61666560b8";
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q= ";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -11,12 +13,15 @@ const response = await fetch (apiUrl + city + `&appid=${apiKey}`);
 
 var data = await response.json();
 
-console.log(data); 
+console.log(data)
+
 
 document.querySelector(".city"). innerHTML= data.name;
 document.querySelector(".temp"). innerHTML= Math.round(data.main.temp) + "°c";
 document.querySelector(".humidity"). innerHTML= data.main.humidity + "%";
 document.querySelector(".wind"). innerHTML= data.wind.speed + "km/hr";
+
+
 
 if (data.weather[0].main == "clouds")  
     {weatherIcon.src="Assets/cloud.png";
@@ -39,7 +44,9 @@ else if (data.weather[0].main == "drizzle")
 {weatherIcon.src="Assets/drizzling.png";
 }
 
+document.querySelector(".weather").style.display="block";
+
 }
 
  searchBtn.addEventListener("click",()=>{ 
-checkWeather(searchBox.value);})
+checkWeather(searchBox.value);}) 
